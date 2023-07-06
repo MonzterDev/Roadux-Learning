ab63b25be83f8dfdf7f2e74887794c7d6968d38d
Monzter <Dancull47@gmail.com>
2023-07-06 12:06:54 -0400

    - Fixed client side reducers

--- src/client/rodux/reducers.ts
+++ src/client/rodux/reducers.ts
@@ -11,48 +11,54 @@ import {
 	UpdateSettingAction,
 } from "shared/types/Rodux";
 import { Pet, PetInstance, Rarity } from "shared/constants/Pets";
+import Object from "@rbxts/object-utils";
 
 export function addPlayer(state: DataState, action: AddPlayerAction): DataState {
 	return action.data;
 }
 
 export function updateCurrency(state: DataState, action: UpdateCurrencyAction): DataState {
-	state.currency[action.currency as keyof typeof state.currency] = action.amount;
-	return state;
+	const newState = Object.deepCopy(state);
+	newState.currency[action.currency as keyof typeof state.currency] = action.amount;
+	return newState;
 }
 
 export function updateSetting(state: DataState, action: UpdateSettingAction): DataState {
-	state.settings[action.setting] = action.value;
-	return state;
+	const newState = Object.deepCopy(state);
+	newState.settings[action.setting] = action.value;
+	return newState;
 }
 
 export function givePet(state: DataState, action: GivePetAction): DataState {
+	const newState = Object.deepCopy(state);
 	const petInstance = action.pet;
-	state.petInventory[petInstance.uuid] = petInstance;
-	return state;
+	newState.petInventory[petInstance.uuid] = petInstance;
+	return newState;
 }
 
 export function updatePet(state: DataState, action: UpdatePetAction): DataState {
-	const pet = state.petInventory[action.uuid];
-	if (!pet) return state;
+	const newState = Object.deepCopy(state);
+	const pet = newState.petInventory[action.uuid];
+	if (!pet) return newState;
 
 	if (action.equipped !== undefined) pet.equipped = action.equipped;
 	if (action.locked !== undefined) pet.locked = action.locked;
 
 	if (action.delete) {
-		state.petInventory[action.uuid] = undefined as never as PetInstance;
+		newState.petInventory[action.uuid] = undefined as never as PetInstance;
 	}
 
-	return state;
+	return newState;
 }
 
 export function renamePet(state: DataState, action: RenamePetAction): DataState {
-	const pet = state.petInventory[action.uuid];
-	if (!pet) return state;
+	const newState = Object.deepCopy(state);
+	const pet = newState.petInventory[action.uuid];
+	if (!pet) return newState;
 
-	state.petInventory[action.uuid].name = action.name;
+	newState.petInventory[action.uuid].name = action.name;
 
-	return state;
+	return newState;
 }
 
 export type DataState = PlayerData;
